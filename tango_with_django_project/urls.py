43deb22ab3b0d3f513366c9ae799aeb2c8b40512
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-01-21 19:13:40 +0200

    Finished Chapter 3. Tests pass.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,7 +20,9 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    
     path('rango/', include('rango.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango   
+    # The above maps any URLs starting with rango/ to be handled by rango
+    
     path('admin/', admin.site.urls),
 ]
