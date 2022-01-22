8bcb386d3adfb48e1241d50bd0587acf03517278
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-01-22 23:16:23 +0200

    Chapter 4 completed. Static & Media

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def index(request):
     #Rango says hey there partner!
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     #Rango says here is the about page.
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+    return render(request, 'rango/about.html')
